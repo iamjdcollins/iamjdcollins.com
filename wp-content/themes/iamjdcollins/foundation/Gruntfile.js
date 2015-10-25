2461d754a622140088f61b3aaeed0b512e9df10b
iamjdcollins <jd@iamjdcollins.com>
2015-10-25 21:15:38 +0000

    Added Grunt Dependency: grunt-contrib-sass

--- wp-content/themes/iamjdcollins/foundation/Gruntfile.js
+++ wp-content/themes/iamjdcollins/foundation/Gruntfile.js
@@ -9,6 +9,9 @@ module.exports = function(grunt) {
           // includes files within path
           {src: ['stylesheets/app.css'], dest: '../css/foundation.css'}, 
           {src: ['bower_components/modernizr/modernizr.js'], dest: '../js/modernizr.js'},
+          {src: ['bower_components/jquery/dist/jquery.min.js'], dest: '../js/jquery.min.js'},
+          {src: ['bower_components/foundation/foundation.min.js'], dest: '../js/foundation.min.js'},
+          {src: ['js/app.js'], dest: '../js/foundation.app.js'},
         ],
       },
     },
