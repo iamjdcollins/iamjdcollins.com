ad8ac0893cc63e8c72eab7b0b3f884faa9847ced
iamjdcollins <jd@iamjdcollins.com>
2015-10-27 22:19:20 -0600

    Troubleshooting modernizr load

--- wp-content/themes/iamjdcollins/front-page.php
+++ wp-content/themes/iamjdcollins/front-page.php
@@ -2,5 +2,5 @@
 /*Use for the front page of the site.*/
 ?>
     <?php get_header(); ?>
-    <h2>Hello Front</h2>
+    <h2>Hello Front: <?php get_template_directory_uri(); ?></h2>
     <?php get_footer(); ?>
