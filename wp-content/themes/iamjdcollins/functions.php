97ac36bd97016b97945a163d9c3b65ab0f4e228a
iamjdcollins <jd@iamjdcollins.com>
2015-10-27 22:44:21 -0600

    Finish adding recommended resources from Foundation 5 (js and css)

--- wp-content/themes/iamjdcollins/functions.php
+++ wp-content/themes/iamjdcollins/functions.php
@@ -2,8 +2,13 @@
 /**
  * Proper way to enqueue scripts and styles
  */
-function iamjdcollins_modernizr() {
-  wp_enqueue_script( 'f5modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), '1.0.0', false );
+function iamjdcollins_foundation5() {
+  wp_enqueue_style('f5normalizemincss', get_template_directory_uri() . '/css/normalize.min.js', array(), '3.0.3','all');
+  wp_enqueue_style('f5foundationmincss', get_template_directory_uri() . '/css/foundation.min.js', array(), '5.5.3','all');
+  wp_enqueue_script( 'f5modernizrminjs', get_template_directory_uri() . '/js/modernizr.min.js', array(), '2.8.3', false );
+  wp_enqueue_script( 'f5jqueryminjs', get_template_directory_uri() . '/js/jquery.min.js', array(), '2.1.4', true );
+  wp_enqueue_script( 'f5foundationminjs', get_template_directory_uri() . '/js/foundation.min.js', array(), '5.5.3', true );
+  wp_enqueue_script( 'f5foundationminappjs', get_template_directory_uri() . '/js/foundation.app.min.js', array(), '5.5.3', true );
 }
 
-add_action( 'wp_enqueue_scripts', 'iamjdcollins_modernizr' );
+add_action( 'wp_enqueue_scripts', 'iamjdcollins_foundation5' );
