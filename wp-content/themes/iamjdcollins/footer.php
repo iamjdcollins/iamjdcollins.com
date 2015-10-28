72e62f535ceacfcfd4b77d5a4f6baa54b693e0c1
iamjdcollins <jd@iamjdcollins.com>
2015-10-27 21:49:35 -0600

    Add minimum index.php and footer.php to test the template.

--- wp-content/themes/iamjdcollins/footer.php
+++ wp-content/themes/iamjdcollins/footer.php
@@ -1,3 +1,6 @@
 <?php
 /*Use for the footer of each page.*/
 ?>
+  <?php wp_footer(); ?>
+  <body>
+</html>
--- wp-content/themes/iamjdcollins/index.php
+++ wp-content/themes/iamjdcollins/index.php
@@ -1,4 +1,6 @@
 <?php
 /*Fall back template for all pages that do not have a more specific template definition*/
-
 ?>
+    <?php get_header(); ?>
+    <h2>Hello</h2>
+    <?php get_footer(); ?>
