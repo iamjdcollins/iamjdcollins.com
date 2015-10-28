3e8e2b775c70504d80acc797f84330288dddfc8f
iamjdcollins <jd@iamjdcollins.com>
2015-10-27 21:41:00 -0600

    Create the functions.php and enqueue modernizr

---
+++ wp-content/themes/iamjdcollins/functions.php
@@ -0,0 +1,8 @@
+/**
+ * Proper way to enqueue scripts and styles
+ */
+function iamjdcollins_modernizr() {
+  wp_enqueue_script( 'modernizr', get_template_directory_uri() . '/js/modernizr.min.js', array(), '1.0.0', false );
+}
+
+add_action( 'wp_enqueue_scripts', 'iamjdcollins_modernizr' );
