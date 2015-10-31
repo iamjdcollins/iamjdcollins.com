b04b388ce51fbd69df54ee36de867ea72b878c5e
iamjdcollins <jd@iamjdcollins.com>
2015-10-31 15:35:38 -0600

    Change to intro

--- wp-content/themes/iamjdcollins/front-page.php
+++ wp-content/themes/iamjdcollins/front-page.php
@@ -3,7 +3,6 @@
 ?>
     <?php get_header(); ?>
     <!--<h2>Template: <?php get_template_directory_uri(); ?></h2>-->
-    <section id="page_image">
-      <img src="http://gethdwallpapers.net/wallpapers/l/1920x1080/65/abstract_minimalistic_grayscale_geometry_digital_art_triangles_1920x1080_64552.jpg">
+    <section id="intro">
     </section>
     <?php get_footer(); ?>
