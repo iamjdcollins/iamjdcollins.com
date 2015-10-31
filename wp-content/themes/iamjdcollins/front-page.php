8870124c271c89a97c13affbfd8f05142c2e850b
iamjdcollins <jd@iamjdcollins.com>
2015-10-31 14:20:00 -0600

    Add Page Image

--- wp-content/themes/iamjdcollins/front-page.php
+++ wp-content/themes/iamjdcollins/front-page.php
@@ -2,5 +2,8 @@
 /*Use for the front page of the site.*/
 ?>
     <?php get_header(); ?>
-    <h2>Hello Front: <?php get_template_directory_uri(); ?></h2>
+    <!--<h2>Template: <?php get_template_directory_uri(); ?></h2>-->
+    <section id="page_image">
+      <img src="http://gethdwallpapers.net/wallpapers/l/1920x1080/65/abstract_minimalistic_grayscale_geometry_digital_art_triangles_1920x1080_64552.jpg">
+    </section>
     <?php get_footer(); ?>
