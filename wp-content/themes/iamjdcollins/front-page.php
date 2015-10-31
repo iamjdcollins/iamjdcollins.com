1f91195cceadd040787e52ce5b40e5abf4c4985c
iamjdcollins <jd@iamjdcollins.com>
2015-10-31 17:02:34 -0600

    Test dynamic page intro image

--- wp-content/themes/iamjdcollins/front-page.php
+++ wp-content/themes/iamjdcollins/front-page.php
@@ -4,9 +4,9 @@
     <?php get_header(); ?>
     <!--<h2>Template: <?php get_template_directory_uri(); ?></h2>-->
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
-    <section id="intro">
-      <?php the_content(); ?>
+    <section id="intro" style="background: transparent url('" . http://365psd.com/images/previews/d29/shards-geometric-pattern-image-2036shards_pattern1.jpg . "') no-repeat scroll 0px 0px / cover">
     </section>
+    <?php the_content(); ?>
     <?php endwhile; else : ?>
     <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
     <?php endif; ?>
