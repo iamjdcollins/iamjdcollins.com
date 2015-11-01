f3927c89520c2aa1fcb6e07db9a6c05523d1fec5
iamjdcollins <jd@iamjdcollins.com>
2015-10-31 18:26:43 -0600

    Add intro text

--- wp-content/themes/iamjdcollins/front-page.php
+++ wp-content/themes/iamjdcollins/front-page.php
@@ -4,8 +4,13 @@
     <?php get_header(); ?>
     <!--<h2>Template: <?php get_template_directory_uri(); ?></h2>-->
     <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+    <?php if ( get_field('page_intro' == 'Yes') ): ?>
     <section id="intro" style="background: transparent url(<?php if(get_field('background_image')){ echo get_field('background_image');} ?>) no-repeat scroll 0px 0px / cover">
+      <?php if ( get_field('into_text') ): ?>
+      <p><?php echo get_field('into_text'); ?></p>
+      <?php endif; ?>
     </section>
+    <?php endif; ?>
     <main>
       <div class="row page-body">
         <div class="small-12 columns">
