ff093e5086fea4e5b723836ec841e700e3668689
iamjdcollins <jd@iamjdcollins.com>
2015-11-28 21:25:11 -0700

    Started Book Reviews Plugin

---
+++ wp-content/plugins/iamjdcollins-book-reviews/iamjdcollins-book-review.php
@@ -0,0 +1,13 @@
+<?php 
+/*
+Plugin Name: Jd Collins Book Reviews
+Plugin URI:  http://www.iamjdcollins/bookreviews
+Description: Used for book reviews
+Version:     1.0
+Author:      Jd Collins
+Author URI:  http://www.iamjdcollins.com
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: iamjdcollins-book-reviews
+*/
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
