ac54e6b120e0ff1f87fc31848cb37da35a25db1e
iamjdcollins <jd@iamjdcollins.com>
2015-11-28 22:17:06 -0700

    Trying to get the menu item working

--- wp-content/plugins/iamjdcollins-book-reviews/iamjdcollins-book-reviews.php
+++ wp-content/plugins/iamjdcollins-book-reviews/iamjdcollins-book-reviews.php
@@ -42,7 +42,7 @@ function iamjdcollins_book_reviews_init() {
 		'capability_type'    => 'post',
 		'has_archive'        => true,
 		'hierarchical'       => false,
-		'menu_position'      => null,
+		'menu_position'      => 25,
 		'supports'           => array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' )
 	);
 
