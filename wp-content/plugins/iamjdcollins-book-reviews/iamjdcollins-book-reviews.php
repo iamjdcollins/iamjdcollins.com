355e997c1ff43d87129ee24e50ed339f2dc60e83
iamjdcollins <jd@iamjdcollins.com>
2015-11-29 00:25:01 -0700

    Fix value not being saved issue on book review author field

--- wp-content/plugins/iamjdcollins-book-reviews/iamjdcollins-book-reviews.php
+++ wp-content/plugins/iamjdcollins-book-reviews/iamjdcollins-book-reviews.php
@@ -65,7 +65,7 @@ function iamjdcollins_book_review_author_metabox() {
 
 	echo '<input class="book-author" type="text" name="book_author" value="' . get_post_meta( $post->ID, 'book_review_author', true ) . '" />';
 
-	update_post_meta($post->ID, $meta_key, $value );
+	update_post_meta($post->ID, 'book_review_author', 'Jd Collins2' );
 
 }
 
